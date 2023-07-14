6f0abcfc80f1b0de3da44aea54a1522347927580
ATUL TINGRE <atultingre.work@gmail.com>
2023-07-14 21:15:46 +0530

    baseurl updated

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://expense-tracker-backend-7c9c.onrender.com"
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://expense-tracker-backend-7c9c.onrender.com"
+// export const BASE_URL = "http://localhost:3000"
